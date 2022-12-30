0b95adb57ac46d40a3d532ffbc15a889c5a308ab
SarinaMashreghi <sarina.m1384@gmail.com>
2022-12-29 23:52:19 -0500

    trying to fix the camera activity

--- app/src/main/java/com/example/monkeypoxdetection/MainActivity.java
+++ app/src/main/java/com/example/monkeypoxdetection/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -19,10 +21,13 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.example.monkeypoxdetection.ml.Efficientnnetb7ModelOptimized;
-import com.example.monkeypoxdetection.ml.MonkeypoxModelNet3;
+
+//import com.example.monkeypoxdetection.ml.EfficientnnNetb7ModelOptimized;
+
 
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.image.TensorImage;
@@ -40,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 //    TextView txt;
     ArrayList<String> labels;
     Uri selectedImage;
+    final int PERMISSION_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -146,12 +152,12 @@ public class MainActivity extends AppCompatActivity {
 //            MonkeypoxModelNet3.Outputs outputs = model.process(inputFeature0);
 //            float[] outputFeature0 = outputs.getOutputFeature0AsTensorBuffer().getFloatArray();
 
-            Efficientnnetb7ModelOptimized model = Efficientnnetb7ModelOptimized.newInstance(this);
+            EfficientnnNetb7ModelOptimized model = EfficientnnNetb7ModelOptimized.newInstance(this);
 
             TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
             inputFeature0.loadBuffer(byteBuffer);
 
-            Efficientnnetb7ModelOptimized.Outputs outputs = model.process(inputFeature0);
+            Efficientnnnetb7ModelOptimized.Outputs outputs = model.process(inputFeature0);
             float[] outputFeature0 = outputs.getOutputFeature0AsTensorBuffer().getFloatArray();
 //            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
 
@@ -190,13 +196,109 @@ public class MainActivity extends AppCompatActivity {
         return ind;
     }
 
-    public void openCamera(View v){
-        Intent i = new Intent(this, Camera.class);
-        startActivity(i);
-    }
-    
+    public void openCamera(View v) {
+//        Intent i = new Intent(this, Camera.class);
+//        startActivity(i);
 
-    public void setImage(Uri image_uri){
+//        ImageButton captureBtn;
+//        ImageView image;
+//        Uri image_uri;
+//        Bitmap bitmap;
+
+//        @Override
+//        protected void onCreate(Bundle savedInstanceState) {
+//            super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_camera);
+//
+//            image = findViewById(R.id.imgView);
+//            captureBtn = findViewById(R.id.captureBtn);
+//        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED) {
+                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permission, PERMISSION_CODE);
+
+
+            } else {
+                takePic();
+            }
+        } else {
+            takePic();
+        }
+    }
+
+    public void takePic() {
+        ImageButton captureBtn;
+        ImageView image;
+        Uri image_uri;
+        Bitmap bitmap;
+
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        image = findViewById(R.id.imgView);
+        captureBtn = findViewById(R.id.captureBtn);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        ActivityResultLauncher<Intent> someActivityResultLauncher;
+        someActivityResultLauncher.launch(cameraIntent);
+
+
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+
+                            image.setImageURI(image_uri);
+
+
+                            Bundle b = new Bundle();
+                            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+
+
+//                        try {
+//                            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), image_uri);
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+
+//                        b.putParcelable("image", bitmap);
+                            i.putExtra("image_uri", image_uri);
+                            startActivity(i);
+
+                        }
+                    }
+                });
+
+        public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults){
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            switch (requestCode) {
+                case PERMISSION_CODE: {
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        takePic();
+                    } else {
+                        Toast.makeText(this, "Permission denied ...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void setImage (Uri image_uri){
         img.setImageURI(image_uri);
     }
+
 }
+
